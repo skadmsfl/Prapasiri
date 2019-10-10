4817ffba7f2b6a7e4e10f1c12658e70eb45801e9
skadmsfl <noeytt.pk@gmail.com>
2019-10-10 22:29:15 +0700

    MainActivity

--- app/src/main/java/com/example/prapasiri/MainActivity.kt
+++ app/src/main/java/com/example/prapasiri/MainActivity.kt
@@ -2,11 +2,54 @@ package com.example.prapasiri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var usersDBHelper: UsersDBHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usersDBHelper = UsersDBHelper(this)
+    }
+
+    fun addUser(v: View) {
+        var userid = this.edittext_userid.text.toString()
+        var name = this.edittext_name.text.toString()
+        var age = this.edittext_age.text.toString()
+        var result = usersDBHelper.insertUser(UserModel(userid = userid, name = name, age = age))
+
+        this.edittext_age.setText("")
+        this.edittext_name.setText("")
+        this.edittext_userid.setText("")
+        this.textview_result.setText = "Added user : " + result
+        this.ll_entries.removeAllViews()
+    }
+
+    fun deleteUser(v: View) {
+        var userid = this.edittext_userid.text.toString()
+        val result = usersDBHelper.deleteUser(userid)
+        this.textview_result.text = "Deleted user : " + result
+        this.ll_entries.removeAllViews()
+    }
+
+    fun showAllUser(v: View){
+        var user = usersDBHelper.readAllUsers()
+        this.ll_entries.removeAllViews()
+        users.forEach {
+            var tv_user = TextView(this)
+            tv_user.textSize = 30F
+            tv_user.text =
+                "Id: " + it.userid.toString()+"\n" + "Name: " + it.name.toString() + "  Age: " + it.age.toString()+"\n"
+            this.ll_entries.addView(tv_user)
+        }
+        this.textview_reslt.text = "Fetched " + users.size + " users"
+
     }
 }
+
+
