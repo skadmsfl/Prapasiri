34032e2d96f48ce4304754b685b40488b8b33c45
skadmsfl <noeytt.pk@gmail.com>
2019-10-10 23:12:24 +0700

    showdata

---
+++ app/src/main/java/com/example/prapasiri/showdata.kt
@@ -0,0 +1,14 @@
+package com.example.prapasiri
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.showdata.*
+
+class showdat : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.showdata)
+    }
+}
+
